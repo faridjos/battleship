d72774b15174721930fb535417ffb3285813864a
faridjos <farid.benachenhou@hotmail.com>
2022-03-08 09:49:49 +0000

    Add basic structureof application

--- run.py
+++ run.py
@@ -1,2 +1,33 @@
 from random import randint
-print(f"Hello {randint(0,10)}")
+
+class ships:
+    grid_size = 0
+    Number_of_ships = 0
+    def __init__(self, name):
+        self.name = name
+    def position():
+        coordinates = []
+
+def print_game_info()
+
+def place_ships()
+
+def guess_coordinates()
+
+def is_data_valid()
+
+def display_hit_or_miss()
+
+def display_score()
+
+print_game_info()
+
+my_ships.position()
+
+while repeat and !noship:
+    while not is_data_valid():
+        guess_coordinates()
+    display_hit_or_miss()
+    display_score()
+        
+
