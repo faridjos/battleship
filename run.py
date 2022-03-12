2c66c87ce7c0edf5b8fa10805267e238835cfebf
faridjos <farid.benachenhou@hotmail.com>
2022-03-12 00:09:44 +0000

    Validate data: name in init method in Class Human; in method input_coord in Class HumanPlayer; check that coordinates do not come twice in coordinates, guesses in Class Ships and my_guesses in Classes HumanPlayer and ComputerPlayer. Add methods rand_coord add_xy_to_list and hit in Class Ships. Add instance variable score in Class Ships. Test if all new functions work

--- run.py
+++ run.py
@@ -11,20 +11,44 @@ class Ships:
     def __init__(self):
         self.coordinates = []
         self.guesses = []
+        self.score = 0
+
+    def rand_coord(self):
+        """generates random position
+        """
+        x = randint(0, Ships.grid_size - 1)
+        y = randint(0, Ships.grid_size - 1)
+
+        return (x, y)
+
+    def add_xy_to_list(self, fun, array):
+        """
+        Add coordinates to a list
+        """
+        while True:
+            (x, y) = fun()
+            if (x, y) not in array:
+                array.append((x, y))
+                break        
 
     def position_ships(self):
         """
         Generate random positions for ships
         """
         for _ in range(Ships.number_of_ships):
-            self.coordinates.append((randint(0, Ships.grid_size - 1), randint(0, Ships.grid_size - 1)))
+            self.add_xy_to_list(self.rand_coord, self.coordinates)
+
         print(self.coordinates)
 
-    
-    #@property
-    #def guess_list(self):
-        
-    #    return self._guess_list
+    def hit(self):
+        """Checks if opponent hit a ship"
+        """
+        for (x, y) in self.coordinates:
+            hit = self.guesses[-1] == (x, y)
+            if hit:
+                self.score += 1
+                return True
+        return hit
 
 
 class Human:
@@ -35,7 +59,11 @@ class Human:
         """
         User inputs name
         """
-        self.name = input("Enter your name:\n")
+        while True:
+            name = input("Enter your name:\n")
+            if name != '':
+                break
+        self.name = name
 
 
 class HumanPlayer(Ships, Human):
@@ -46,27 +74,57 @@ class HumanPlayer(Ships, Human):
         Ships.__init__(self)
         self.my_guesses = []
 
+    def input_coord(self):
+        """User enters ship coordinates which are then validated
+        """
+        while True:
+            x = input("Guess x-coordinate:\n")
+            if x.isdigit():
+                x = int(x)
+                if 0 <= x < self.grid_size:
+                    break
+                else:
+                    print(f"The coordinates must be between 0 and {self.grid_size - 1}")
+            else:
+                print("You must enter a number!")
+
+        while True:
+            y = input("Guess y-coordinate:\n")
+            if y.isdigit():
+                y = int(y)
+                if 0 <= y < self.grid_size:
+                    break
+                else:
+                    print(f"The coordinates must be between 0 and {self.grid_size - 1}")
+            else:
+                print("You must enter a number!")
+
+        return (x, y)
+
     def guess_coordinates(self):
         """User enters opponent ship coordinates to try to hit
         """
-        x = input("Guess x-coordinate:\n")
-        y = input("Guess y-coordinate:\n")
-        self.my_guesses.append((int(x), int(y)))
-        #print(self.my_guesses)
+        while True:
+            (x, y) = self.input_coord()
+            if (x, y) not in self.my_guesses:
+                self.my_guesses.append((x, y))
+                break
+            else:
+                print(f"You already guessed position {(x, y)}, try again")
 
     def print_grid(self):
         """
-        Print grid. Ships are shown as "@" and hits as "X"
+        Print grid. Ships are shown as "@" and shots as "X"
         """
         for i in range(Ships.grid_size):
             for j in range(Ships.grid_size):
-                hit = False
+                shot = False
                 for (x, y) in self.guesses:
-                    hit = (i, j) == (x, y)
-                    if hit:
+                    shot = (i, j) == (x, y)
+                    if shot:
                         print('X ', end=' ')
                         break
-                if not hit:
+                if not shot:
                     found_ship = False
                     for (x, y) in self.coordinates:
                         found_ship = (i, j) == (x, y)
@@ -74,7 +132,7 @@ class HumanPlayer(Ships, Human):
                             print('@ ', end=' ')
                             break
                     if not found_ship:
-                        print('. ', end=' ')                     
+                        print('. ', end=' ')                   
             print('')
 
 
@@ -88,22 +146,22 @@ class ComputerPlayer(Ships):
     def guess_coordinates(self):
         """Computer generates random opponent ship coordinates to try to hit
         """
-        self.my_guesses.append((randint(0, Ships.grid_size - 1), randint(0, Ships.grid_size - 1)))
+        self.add_xy_to_list(self.rand_coord, self.my_guesses)
         #print(self.my_guesses)
 
     def print_grid(self):
         """
-        Print grid. Ships are not shown, hits are shown as 'X'
+        Print grid. Ships are not shown, shots are shown as 'X'
         """
         for i in range(Ships.grid_size):
             for j in range(Ships.grid_size):
-                hit = False
+                shot = False
                 for (x, y) in self.guesses:
-                    hit = (i, j) == (x, y)
-                    if hit:
+                    shot = (i, j) == (x, y)
+                    if shot:
                         print('X ', end=' ')
                         break
-                if not hit:
+                if not shot:
                     print('. ', end=' ')                     
             print('')
 
@@ -111,21 +169,13 @@ class ComputerPlayer(Ships):
 def print_game_info():
     print("Welcome to the Battleship game!\n")
 
-def is_data_valid():
-    return
-
-def display_hit_or_miss():
-    return
-
-def display_score():
-    return
 
 #print_game_info()
 
 #while repeat and !noship:
  #   while not is_data_valid():
  #       guess_coordinates()
-#    display_hit_or_miss()
+#    display_shot_or_miss()
   #  display_score()
 
 
@@ -140,12 +190,49 @@ print('')
 
 computer.guess_coordinates()
 player.guesses = computer.my_guesses
-print(f"Computer guessed {player.guesses}")
+print(f"Computer guessed {player.guesses[-1]}")
+if player.hit():
+    print("Computer hit")
+else:
+    print("Computer missed")
 print('')
 
 player.guess_coordinates()
 computer.guesses = player.my_guesses
-print(f"{player.name} guessed {computer.guesses}")
+print(f"{player.name} guessed {computer.guesses[-1]}")
+if computer.hit():
+    print("Player hit")
+else:
+    print("Player missed")
+print('')
+
+print(f"score: Computer {player.score} Player {computer.score}")
+print('')
+
+player.print_grid()
+print('')
+computer.print_grid()
+
+
+computer.guess_coordinates()
+player.guesses = computer.my_guesses
+print(f"Computer guessed {player.guesses[-1]}")
+if player.hit():
+    print("Computer hit")
+else:
+    print("Computer missed")
+print('')
+
+player.guess_coordinates()
+computer.guesses = player.my_guesses
+print(f"{player.name} guessed {computer.guesses[-1]}")
+if computer.hit():
+    print("Player hit")
+else:
+    print("Player missed")
+print('')
+
+print(f"score: Computer {player.score} Player {computer.score}")
 print('')
 
 player.print_grid()
