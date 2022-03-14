2becdbfce6073dc575de8ca9110d2bd294f50a8a
faridjos <farid.benachenhou@hotmail.com>
2022-03-14 03:13:14 +0000

    Add test print

--- run.py
+++ run.py
@@ -38,7 +38,7 @@ class Ships:
         for _ in range(Ships.number_of_ships):
             self.add_xy_to_list(self.rand_coord, self.coordinates)
 
-        #print(self.coordinates)
+        print(self.coordinates)
 
     def hit(self):
         """Checks if opponent hit a ship"
